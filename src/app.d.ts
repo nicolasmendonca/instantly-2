090251b96cf648c7788337eb0610a57b64a1e049
nikomendo <nicolasmendonca@gmail.com>
2023-01-26 15:16:30 -0300

    ⚡️ Adds supabase

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,20 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { TypedSupabaseClient } from '@supabase/auth-helpers-sveltekit/dist/types';
+import type { Session } from "@supabase/supabase-js"
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
+		interface Locals {
+			supabase: TypedSupabaseClient;
+			session: Session | null;
+		}
+
+		interface PageData {
+			session: Session | null
+		}
 		// interface Platform {}
 	}
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
-<script>
+<script lang="ts">
+	import { supabaseClient } from '../infrastructure/supabase';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
 	import '../app.postcss';
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabaseClient.auth.onAuthStateChange(() => {
+			invalidate('supabase:auth');
+		});
+
+		return () => {
+			subscription.unsubscribe();
+		};
+	});
 </script>
 
 <slot />
