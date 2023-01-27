ab4cb99cd06b6ca4ddd79df56a88d452ac694f5c
nikomendo <nicolasmendonca@gmail.com>
2023-01-27 10:00:08 -0300

    Adds basic registration

---
+++ src/routes/auth/signup/+page.server.ts
@@ -0,0 +1,24 @@
+import type { Actions } from './$types';
+import { zfd } from 'zod-form-data'
+import { z } from 'zod'
+import { fail } from '@sveltejs/kit';
+
+const loginRequestSchema = zfd.formData({
+  email: z.string(),
+  password: z.string(),
+})
+
+export const actions = ({
+  register: async ({ request, locals }) => {
+    const { supabase } = locals;
+    const { email, password } = loginRequestSchema.parse(await request.formData());
+
+    const { data, error } = await supabase.auth.signUp({ email, password })
+
+    if (error) {
+      return fail(401, {incorrect: true, message: error.message})
+    }
+
+    return data.user
+  }
+}) satisfies Actions;
